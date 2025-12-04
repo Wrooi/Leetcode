3ead4ef5f5c16d36da9a225098eefffbb5536b7f
Wrooi <ooiwenree@gmail.com>
2025-12-04 22:48:17 +0800

    feat: Add Daily/Count_Collisions_on_a_Road_2211 solution

---
+++ src/Daily/Count_Collisions_on_a_Road_2211.java
@@ -0,0 +1,19 @@
+package Daily;
+
+public class Count_Collisions_on_a_Road_2211 {
+    class Solution {
+        public int countCollisions(String directions) {
+            int n = directions.length(), ans = 0, l = 0, r = n - 1;
+            while(l < n && directions.charAt(l) == 'L') l++;
+            while(r >= l && directions.charAt(r) == 'R') r--;
+            for(int i = l ; i <= r; i++) {
+                if(directions.charAt(i) != 'S') ans++;
+            }
+            return ans;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
