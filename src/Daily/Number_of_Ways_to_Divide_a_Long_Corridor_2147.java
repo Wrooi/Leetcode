299d0ba0b44130f1a0f02ef75908e5acdfb87256
Wrooi <ooiwenree@gmail.com>
2025-12-14 19:40:33 +0800

    feat: Add Daily/Number_of_Ways_to_Divide_a_Long_Corridor_2147 solution

---
+++ src/Daily/Number_of_Ways_to_Divide_a_Long_Corridor_2147.java
@@ -0,0 +1,34 @@
+package Daily;
+
+import java.util.ArrayList;
+
+public class Number_of_Ways_to_Divide_a_Long_Corridor_2147 {
+    class Solution {
+        public int numberOfWays(String corridor) {
+            int mod = 1_000_000_007;
+            ArrayList<Integer> pos = new ArrayList<>();
+
+            for (int i = 0; i < corridor.length(); i++) {
+                if (corridor.charAt(i) == 'S') {
+                    pos.add(i);
+                }
+            }
+
+            if (pos.size() % 2 == 1 || pos.size() == 0) {
+                return 0;
+            }
+
+            long res = 1;
+            for (int i = 2; i < pos.size(); i += 2) {
+                int len_of_gap = pos.get(i) - pos.get(i - 1);
+                res = (res * len_of_gap) % mod;
+            }
+
+            return (int) res;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
