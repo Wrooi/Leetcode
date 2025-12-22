91ab7be3e36b46b0bb681f8eab819fc746bae4dc
Wrooi <ooiwenree@gmail.com>
2025-12-22 19:03:38 +0800

    feat: Add Daily/Delete_Columns_to_Make_Sorted_III_960 solution

---
+++ src/Daily/Delete_Columns_to_Make_Sorted_III_960.java
@@ -0,0 +1,34 @@
+package Daily;
+
+import java.util.Arrays;
+
+public class Delete_Columns_to_Make_Sorted_III_960 {
+    class Solution {
+        public int minDeletionSize(String[] strs) {
+            int n = strs[0].length(), m = strs.length;
+            int[] dp = new int[n];
+            Arrays.fill(dp, 1);
+            for (int i = 1; i < n; i++) {
+                for (int j = 0; j < i; j++) {
+                    if (isValid(strs, j, i)) {
+                        dp[i] = Math.max(dp[i], dp[j] + 1);
+                    }
+                }
+            }
+            int max = 0;
+            for (int val : dp) max = Math.max(max, val);
+            return n - max;
+        }
+
+        private boolean isValid(String[] strs, int j, int i) {
+            for (String s : strs) {
+                if (s.charAt(j) > s.charAt(i)) return false;
+            }
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
