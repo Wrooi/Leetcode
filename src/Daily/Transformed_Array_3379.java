8e36305a283115d8aae13c8b642a0cba7a10bc44
Wrooi <ooiwenree@gmail.com>
2026-02-05 21:09:27 +0800

    feat: Add Daily/Transformed_Array_3379 solution

---
+++ src/Daily/Transformed_Array_3379.java
@@ -0,0 +1,19 @@
+package Daily;
+
+public class Transformed_Array_3379 {
+    class Solution {
+        public int[] constructTransformedArray(int[] A) {
+            int n = A.length;
+            int[] res = new int[n];
+
+            for (int i = 0; i < n; i++)
+                res[i] = A[(((i + A[i]) % n) + n) % n];
+
+            return res;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
