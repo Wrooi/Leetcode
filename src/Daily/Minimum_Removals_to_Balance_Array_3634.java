b38a0cdd455c6fd3893cf9947120ab6044372a82
Wrooi <ooiwenree@gmail.com>
2026-02-06 12:21:57 +0800

    feat: Add Daily/Minimum_Removals_to_Balance_Array_3634 solution

---
+++ src/Daily/Minimum_Removals_to_Balance_Array_3634.java
@@ -0,0 +1,27 @@
+package Daily;
+
+import java.util.Arrays;
+
+public class Minimum_Removals_to_Balance_Array_3634 {
+    class Solution {
+        public int minRemoval(int[] nums, int k) {
+            Arrays.sort(nums);
+            int i = 0;
+            int maxLen = 0;
+
+            for (int j = 0; j < nums.length; j++) {
+                // Use long to prevent integer overflow for nums[i] * k
+                while ((long) nums[j] > (long) nums[i] * k) {
+                    i++;
+                }
+                maxLen = Math.max(maxLen, j - i + 1);
+            }
+
+            return nums.length - maxLen;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
