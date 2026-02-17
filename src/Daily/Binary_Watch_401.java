5e06a49f7e01220608db24187bf714f43fd68610
Wrooi <ooiwenree@gmail.com>
2026-02-17 20:42:12 +0800

    feat: Add Daily/Binary_Watch_401 solution

---
+++ src/Daily/Binary_Watch_401.java
@@ -0,0 +1,34 @@
+package Daily;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Binary_Watch_401 {
+    class Solution {
+        public List<String> readBinaryWatch(int turnedOn) {
+            List<String> result = new ArrayList<>();
+
+            for (int hour = 0; hour < 12; hour++) {
+
+                for (int minute = 0; minute < 60; minute++) {
+
+                    int totalBits = Integer.bitCount(hour) + Integer.bitCount(minute);
+
+                    if (totalBits == turnedOn) {
+
+                        String time = hour + ":" +
+                                (minute < 10 ? "0" + minute : minute);
+
+                        result.add(time);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
