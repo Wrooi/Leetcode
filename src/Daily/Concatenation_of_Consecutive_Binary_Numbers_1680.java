01f168a959d127412195b2c091b4329733cd1682
Wrooi <ooiwenree@gmail.com>
2026-02-28 13:09:50 +0800

    feat: Add Daily/Concatenation_of_Consecutive_Binary_Numbers_1680 solution

---
+++ src/Daily/Concatenation_of_Consecutive_Binary_Numbers_1680.java
@@ -0,0 +1,21 @@
+package Daily;
+
+public class Concatenation_of_Consecutive_Binary_Numbers_1680 {
+    class Solution {
+        public int concatenatedBinary(int n) {
+            final int MOD = 1_000_000_007;
+            long res = 0;
+            int bits = 0;
+
+            for(int i=1; i<=n; i++){
+                if((i & (i-1)) == 0) bits++;
+                res = ((res<<bits) | i) % MOD;
+            }
+            return (int)res;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
