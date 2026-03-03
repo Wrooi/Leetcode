e04b251bbeb9e02d4b371538c81eec0e6f21c59e
Wrooi <ooiwenree@gmail.com>
2026-03-03 14:22:13 +0800

    feat: Add Daily/Find_Kth_Bit_in_Nth_Binary_String_1545 solution

---
+++ src/Daily/Find_Kth_Bit_in_Nth_Binary_String_1545.java
@@ -0,0 +1,22 @@
+package Daily;
+
+public class Find_Kth_Bit_in_Nth_Binary_String_1545 {
+    class Solution {
+        public char findKthBit(int n, int k) {
+            if (n == 1) return '0';
+
+            int len = (1 << n) - 1;
+            int mid = (len + 1) / 2;
+
+            if (k == mid) return '1';
+            if (k < mid) return findKthBit(n - 1, k);
+
+            char c = findKthBit(n - 1, len - k + 1);
+            return c == '0' ? '1' : '0';
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
