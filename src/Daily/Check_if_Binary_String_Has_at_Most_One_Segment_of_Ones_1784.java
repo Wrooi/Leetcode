760037cdd92de756fd4a6d5b12699b0eb1dae859
Wrooi <ooiwenree@gmail.com>
2026-03-06 13:15:48 +0800

    feat: Add Daily/Check_if_Binary_String_Has_at_Most_One_Segment_of_Ones_1784 solution

---
+++ src/Daily/Check_if_Binary_String_Has_at_Most_One_Segment_of_Ones_1784.java
@@ -0,0 +1,13 @@
+package Daily;
+
+public class Check_if_Binary_String_Has_at_Most_One_Segment_of_Ones_1784 {
+    class Solution {
+        public boolean checkOnesSegment(String s) {
+            return !s.contains("01");
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
