d64fb970b8d06eae0f44890b6407794cd42707fb
Wrooi <ooiwenree@gmail.com>
2026-03-18 21:13:05 +0800

    feat: Add Daily/Count_Submatrices_with_Top_Left_Element_and_Sum_Less_Than_k_3070 solution

---
+++ src/Daily/Count_Submatrices_with_Top_Left_Element_and_Sum_Less_Than_k_3070.java
@@ -0,0 +1,22 @@
+package Daily;
+
+public class Count_Submatrices_with_Top_Left_Element_and_Sum_Less_Than_k_3070 {
+    class Solution {
+        public int countSubmatrices(int[][] grid, int k) {
+            int m = grid.length, n = grid[0].length;
+            int ans = 0;
+            int[][] px = new int[m + 1][n + 1];
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    px[i+1][j+1] = grid[i][j] + px[i][j+1] + px[i+1][j] - px[i][j];
+                    if (px[i+1][j+1] <= k) ans++;
+                }
+            }
+            return ans;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
