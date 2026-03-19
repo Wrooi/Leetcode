58027f4dc506f8fc4ec3c82ec8bb36569118153b
Wrooi <ooiwenree@gmail.com>
2026-03-20 05:39:16 +0800

    feat: Add Daily/Count_Submatrices_With_Equal_Frequency_of_X_and_Y_3212 solution

---
+++ src/Daily/Count_Submatrices_With_Equal_Frequency_of_X_and_Y_3212.java
@@ -0,0 +1,32 @@
+package Daily;
+
+public class Count_Submatrices_With_Equal_Frequency_of_X_and_Y_3212 {
+    class Solution {
+        public int numberOfSubmatrices(char[][] grid) {
+            int rows = grid.length, cols = grid[0].length;
+            int[] sumX = new int[cols];
+            int[] sumY = new int[cols];
+            int res = 0;
+
+            for (int i = 0; i < rows; i++) {
+                int rx = 0, ry = 0;
+
+                for (int j = 0; j < cols; j++) {
+                    if (grid[i][j] == 'X') rx++;
+                    else if (grid[i][j] == 'Y') ry++;
+
+                    sumX[j] += rx;
+                    sumY[j] += ry;
+
+                    if (sumX[j] > 0 && sumX[j] == sumY[j]) res++;
+                }
+            }
+
+            return res;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
