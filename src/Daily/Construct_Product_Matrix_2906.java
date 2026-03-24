ad84e8ecdcdd1f64f990ddc550c26972dfbb4f13
Wrooi <ooiwenree@gmail.com>
2026-03-24 16:09:23 +0800

    feat: Add Daily/Construct_Product_Matrix_2906 solution

---
+++ src/Daily/Construct_Product_Matrix_2906.java
@@ -0,0 +1,36 @@
+package Daily;
+
+public class Construct_Product_Matrix_2906 {
+    // This solution was implemented with assistance from ChatGPT
+    class Solution {
+
+        public int[][] constructProductMatrix(int[][] grid) {
+            final int MOD = 12345;
+            int n = grid.length;
+            int m = grid[0].length;
+            int[][] p = new int[n][m];
+
+            long suffix = 1;
+            for (int i = n - 1; i >= 0; i--) {
+                for (int j = m - 1; j >= 0; j--) {
+                    p[i][j] = (int) suffix;
+                    suffix = (suffix * grid[i][j]) % MOD;
+                }
+            }
+
+            long prefix = 1;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    p[i][j] = (int) (((long) p[i][j] * prefix) % MOD);
+                    prefix = (prefix * grid[i][j]) % MOD;
+                }
+            }
+
+            return p;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
