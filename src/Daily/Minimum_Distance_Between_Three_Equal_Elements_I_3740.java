82c6f3f5c8a786d0b9edc484c637a67c6feb33b2
Wrooi <ooiwenree@gmail.com>
2026-04-10 18:34:59 +0800

    feat: Add Daily/Minimum_Distance_Between_Three_Equal_Elements_I_3740 solution

---
+++ src/Daily/Minimum_Distance_Between_Three_Equal_Elements_I_3740.java
@@ -0,0 +1,28 @@
+package Daily;
+
+public class Minimum_Distance_Between_Three_Equal_Elements_I_3740 {
+    class Solution {
+        public int minimumDistance(int[] nums) {
+            int len = nums.length;
+            int[] last2 = new int[len];
+            int res = 200;
+
+            for (int i = 0; i < len; i++) {
+                int val = nums[i] - 1;
+                int pos = i + 1, pack = last2[val];
+                int old = pack & 255, cur = pack >> 8;
+
+                last2[val] = cur | (pos << 8);
+
+                if (old > 0)
+                    res = Math.min(res, (pos - old) << 1);
+            }
+
+            return res == 200 ? -1 : res;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
