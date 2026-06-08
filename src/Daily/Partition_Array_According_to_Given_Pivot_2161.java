23c2259530378639ea71c9123e563f3ec6c937d8
Wrooi <ooiwenree@gmail.com>
2026-06-08 23:01:26 +0800

    feat: Add Daily/Partition_Array_According_to_Given_Pivot_2161 solution

---
+++ src/Daily/Partition_Array_According_to_Given_Pivot_2161.java
@@ -0,0 +1,40 @@
+package Daily;
+
+public class Partition_Array_According_to_Given_Pivot_2161 {
+    class Solution {
+        public int[] pivotArray(int[] nums, int pivot) {
+            int n = nums.length;
+            int[] ans = new int[n];
+
+            int left = 0;
+            int right = n - 1;
+
+            int i = 0;
+            int j = n - 1;
+
+            while (i < n) {
+                if (nums[i] < pivot) {
+                    ans[left++] = nums[i];
+                }
+
+                if (nums[j] > pivot) {
+                    ans[right--] = nums[j];
+                }
+
+                i++;
+                j--;
+            }
+
+            // Remaining positions contain pivot
+            while (left <= right) {
+                ans[left++] = pivot;
+            }
+
+            return ans;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
