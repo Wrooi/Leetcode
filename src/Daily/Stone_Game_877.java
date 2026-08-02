91ee7d3fc7c66f6e486240cc5ba8642c7768d32f
Wrooi <ooiwenree@gmail.com>
2026-08-02 18:27:20 +0800

    feat: Add Daily/Stone_Game_877 solution

---
+++ src/Daily/Stone_Game_877.java
@@ -0,0 +1,13 @@
+package Daily;
+
+public class Stone_Game_877 {
+    class Solution {
+        public boolean stoneGame(int[] piles) {
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
