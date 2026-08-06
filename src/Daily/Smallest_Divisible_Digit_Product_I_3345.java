2e68f4f659330cada4e4d68a6300453d0d5686be
Wrooi <ooiwenree@gmail.com>
2026-08-06 23:57:42 +0800

    feat: Add Daily/Smallest_Divisible_Digit_Product_I_3345 solution

---
+++ src/Daily/Smallest_Divisible_Digit_Product_I_3345.java
@@ -0,0 +1,23 @@
+package Daily;
+
+public class Smallest_Divisible_Digit_Product_I_3345 {
+    class Solution {
+        public int smallestNumber(int n, int t) {
+            int q = n / 10, r = n % 10;
+
+            int req = t / gcd(q + (10 - q) / 10, t);
+            int nxt = ((r + req - 1) / req) * req;
+            int x = nxt - (nxt - 10) * (nxt / 10);
+
+            return q * 10 + x;
+        }
+
+        private int gcd(int a, int b) {
+            return (b == 0) ? a : gcd(b, a % b);
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
