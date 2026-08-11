46c5f92238adbaf8fafd4ab42ce1631dfba12239
Wrooi <ooiwenree@gmail.com>
2026-08-11 12:54:00 +0800

    feat: Add Daily/Smallest_Missing_Integer_Greater_Than_Sequential_Prefix_Sum_2996 solution

---
+++ src/Daily/Smallest_Missing_Integer_Greater_Than_Sequential_Prefix_Sum_2996.java
@@ -0,0 +1,34 @@
+package Daily;
+
+public class Smallest_Missing_Integer_Greater_Than_Sequential_Prefix_Sum_2996 {
+    class Solution {
+        public int missingInteger(int[] A) {
+            int sum = A[0];
+            boolean[] seen = new boolean[52];
+            boolean seq = true;
+
+            seen[A[0]] = true;
+
+            for (int i = 1; i < A.length; i++) {
+                if (seq && A[i] == A[i - 1] + 1)
+                    sum += A[i];
+                else {
+                    seq = false;
+                    if (sum > 50)
+                        return sum;
+                }
+                seen[A[i]] = true;
+            }
+
+            for (int i = sum; i < 52; i++)
+                if (!seen[i])
+                    return i;
+
+            return sum;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
