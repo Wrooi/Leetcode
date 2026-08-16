85a6ceaa3b99231fa54f4c138200a982f008d4ba
Wrooi <ooiwenree@gmail.com>
2026-08-16 20:31:16 +0800

    feat: Add Daily/Stone_Game_IX_2029 solution

---
+++ src/Daily/Stone_Game_IX_2029.java
@@ -0,0 +1,21 @@
+package Daily;
+
+public class Stone_Game_IX_2029 {
+    class Solution {
+        public boolean stoneGameIX(int[] stones) {
+            int[] f = {0, 0, 0};
+
+            for (int s : stones)
+                f[s % 3]++;
+
+            if ((f[0] & 1) == 0)
+                return Math.min(f[1], f[2]) > 0;
+
+            return Math.abs(f[1] - f[2]) > 2;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
