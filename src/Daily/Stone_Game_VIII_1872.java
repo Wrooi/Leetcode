b5888a9b31b6910bd87e811093957f57f34971c1
Wrooi <ooiwenree@gmail.com>
2026-08-24 13:17:24 +0800

    feat: Add Daily/Stone_Game_VIII_1872 solution

---
+++ src/Daily/Stone_Game_VIII_1872.java
@@ -0,0 +1,21 @@
+package Daily;
+
+public class Stone_Game_VIII_1872 {
+    class Solution {
+        public int stoneGameVIII(int[] A) {
+            int n = A.length;
+            for (int i = 1; i < n; i++)
+                A[i] += A[i - 1];
+
+            int ans = A[n - 1];
+            for (int i = n - 2; i > 0; i--)
+                ans = Math.max(ans, A[i] - ans);
+
+            return ans;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
