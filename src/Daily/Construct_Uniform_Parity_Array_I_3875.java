96b3fe7a1ac179fab7058e2c51d7bc2797dd9956
Wrooi <ooiwenree@gmail.com>
2026-09-02 13:49:13 +0800

    feat: Add Daily/Construct_Uniform_Parity_Array_I_3875 solution

---
+++ src/Daily/Construct_Uniform_Parity_Array_I_3875.java
@@ -0,0 +1,11 @@
+package Daily;
+
+public class Construct_Uniform_Parity_Array_I_3875 {
+    class Solution {
+        public boolean uniformArray(int[] A) { return true; }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
