f4a45b73c48bf7a83ef38d5623ea44625c6d607f
Wrooi <ooiwenree@gmail.com>
2026-09-14 13:49:25 +0800

    feat: Add Daily/Rectangle_Overlap_836 solution

---
+++ src/Daily/Rectangle_Overlap_836.java
@@ -0,0 +1,14 @@
+package Daily;
+
+public class Rectangle_Overlap_836 {
+    class Solution {
+        public boolean isRectangleOverlap(int[] r1, int[] r2) {
+            return r1[0] < r2[2] && r2[0] < r1[2] &&
+                    r1[1] < r2[3] && r2[1] < r1[3];
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
