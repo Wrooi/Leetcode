9f392d7cd8ffdd5586b2055daa3dbdbe7a4f102f
Wrooi <ooiwenree@gmail.com>
2026-09-15 16:08:14 +0800

    feat: Add Daily/Maximum_Number_of_Non_overlapping_Palindrome_Substrings_2472 solution

---
+++ src/Daily/Maximum_Number_of_Non_overlapping_Palindrome_Substrings_2472.java
@@ -0,0 +1,36 @@
+package Daily;
+
+public class Maximum_Number_of_Non_overlapping_Palindrome_Substrings_2472 {
+    class Solution {
+        public int maxPalindromes(String s, int k) {
+            int n = s.length();
+            if (k == 1)
+                return n;
+
+            int res = 0;
+
+            for (int i = 0; i <= n - k; i++) {
+                if (check(s, i, i + k - 1)) {
+                    res++;
+                    i += k - 1;
+                } else if (i < n - k && check(s, i, i + k)) {
+                    res++;
+                    i += k;
+                }
+            }
+
+            return res;
+        }
+
+        boolean check(String s, int l, int r) {
+            for (; l < r; l++, r--)
+                if (s.charAt(l) != s.charAt(r))
+                    return false;
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
